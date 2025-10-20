c2a57a370405142e9791b7b5baf98a92e50f4da7
hadis <hadisbeigi97@gmail.com>
2025-10-20 12:56:14 +0330

    feat(search):implement search functionality

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,18 +1,36 @@
 import { getProducts } from "../api/products";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useOutletContext } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
+import { useState } from "react";
+
 export async function loader() {
   const { products } = await getProducts();
   return { products };
 }
 export default function Home() {
-  const { products } = useLoaderData();
+  const { products: initialProducts } = useLoaderData();
+  const { filteredProducts, searchQuery } = useOutletContext();
+  const [products, setProducts] = useState(initialProducts || []);
+
+  const list = searchQuery ? filteredProducts : products;
+
   return (
     <div className="p-6 max-w-7xl mx-auto">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+      <div className="flex flex-wrap justify-center gap-x-6 gap-y-5 w-full">
+        {list.length > 0 ? (
+          list.map((product) => (
+            <div
+              key={product.id}
+              className="flex-none w-1/3 md:w-1/4 lg:w-1/5 p-2"
+            >
+              <ProductCard product={product} />
+            </div>
+          ))
+        ) : (
+          <p className="text-gray-400 text-center w-full mt-4">
+            نتیجه‌ای یافت نشد 😔
+          </p>
+        )}
       </div>
     </div>
   );
