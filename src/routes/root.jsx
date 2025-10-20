2a0bebf477dd3647027b26ed1c65b87eb27017c5
hadis <hadisbeigi97@gmail.com>
2025-10-20 17:18:25 +0330

    feat(pagination): add pagination for product list

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,14 +5,17 @@ import { useState } from "react";
 export default function Root() {
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [page, setPage] = useState(0);
+
   return (
     <>
       <Header
         setFilteredProducts={setFilteredProducts}
         setSearchQuery={setSearchQuery}
+        setPage={setPage}
       />
       <main className="max-w-7xl mx-auto px-4">
-        <Outlet context={{ filteredProducts, searchQuery }} />
+        <Outlet context={{ filteredProducts, searchQuery, page, setPage }} />
       </main>
     </>
   );
