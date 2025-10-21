3deb8c356c7b9d8ca28e9e1bf17b22919041e849
hadis <hadisbeigi97@gmail.com>
2025-10-21 13:52:46 +0330

    Fix/update index.jsx in error-page feature

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./root";
 import Home, { loader as homeLoader } from "./Home";
+import ErrorPage from "../error-page/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
